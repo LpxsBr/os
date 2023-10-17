c09f965f94152c84e7237ab73ff7b7a252621d30
LpxsBr <anselmolopes.an@gmail.com>
2023-10-17 17:31:40 -0300

    feat: update user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,8 +7,10 @@ use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
@@ -58,6 +60,7 @@ class UserController extends Controller
         return response(User::all(), 200);
     }
     public function update(Request $request){
+        $user = Auth::user();
         if(!Auth::user()){
             return response([
                 'status' => 'failed',
@@ -65,37 +68,54 @@ class UserController extends Controller
             ], 403);
         }
 
-        try {
-            //code...
-            DB::beginTransaction();
-            $id = Auth::user()->id;
-            $user = User::find($id);
-            // ->update(
-            //     [ 
-            //         'name' => $request['name'],
-            //         'email' => $request['email'],
-            //         'password' => bcrypt($request['password']),
-            //         'is_admin' => 0
-            //     ]
-            // );
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'name' => 'required|string',
+                'email' => [
+                    'required',
+                    'string',
+                    Rule::unique('users')->ignore($user->id),
+                ],
+                'password' => [
+                    Rule::requiredIf($request->has('password')),
+                    'string',
+                    'confirmed'
+                ]
+            ]    
+        );
 
+        // Check if data is valid
+        if ($validator->fails()) {
+            // Create the response
+            $response = [
+                'message' => 'Os dados enviados estão inválidos',
+                'errors' => $validator->errors()
+            ];
+
+            return response($response, 422);
+        }
+
+        // Get the data validated
+        $fields = $validator->validated();
+        try {
+            DB::beginTransaction();
             $response = [
                 'status' => 'ok',
-                'message'=>'usuario atualizado com sucesso :)',
-                'id' => $id,
+                'message'=>'usuario atualizado com sucesso :)',  
                 'user' => $user
             ];
-            // ->update(
-                // // [  
-                    $user->name = $request['name'];
-                    $user->email = $request['email'];
-                    $user->password = bcrypt($request['password']);
-                    $user->i_admin = 0;
-                // ]
-            // );
-            // $user->name = $request['name'];
+                    $user->name = $request['name'] ?? $user->name;
+                    $user->email = $request['email'] ?? $user->email;
+                    if ($request->has('password')) {
+                        $user->password = bcrypt($fields['password']);
+                    }
+                    $user->is_admin = 0;
             $user->save();
-            return response($response, 200);
+        
+            DB::commit();
+        
+            return response($response, 201);
         } catch (Exception $exception) {
             
             $response = [
